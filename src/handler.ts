e609d0c9dded0784847a0b6be4feabd0401a5bf5
Max Schweikart <hello@maxschweik.art>
2021-10-29 23:32:23 +0200

    add html loading

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,14 @@
+import HTML from './ui.html'
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const url = new URL(request.url)
+
+  if (url.pathname === '/') {
+    return new Response(HTML, {
+      headers: {
+        'Content-Type': 'text/html;charset=UTF-8',
+      },
+    })
+  }
+  return new Response(`request method: ${request.method} ${url.pathname}`)
 }
