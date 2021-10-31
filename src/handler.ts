c37b50d348e27229d9f222b766513f757006b426
Max Schweikart <hello@maxschweik.art>
2021-10-31 13:34:38 +0100

    add API route

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,41 @@
 import { Env } from './Env'
-import HTML from './ui.html'
+import UI from './ui.html'
+
+const apiRoute = '/api/game/';
 
 export async function handleRequest(request: Request, env: Env): Promise<Response> {
-  const id = env.GAMEINSTANCE.idFromName('A');
-  const obj = env.GAMEINSTANCE.get(id);
-  const resp = await obj.fetch(request.url);
+  const path = new URL(request.url).pathname;
+  if (path === '/') {
+    return createUiResponse();
+  } else if (path.startsWith(apiRoute)) {
+    return await handleApiRequest(request, env);
+  } else {
+    return createNotFoundResponse();
+  }
+}
+
+function createUiResponse(): Response {
+  return new Response(UI, {
+    headers: {
+      'Content-Type': 'text/html;charset=UTF-8'
+    }
+  });
+}
+
+function createNotFoundResponse(): Response {
+  return new Response('Not found');
+}
+
+async function handleApiRequest(req: Request, env: Env): Promise<Response> {
+  const path = new URL(req.url).pathname;
+
+  const gameName = path.substr(apiRoute.length);
+  if (!/^[0-9a-zA-Z]+$/.test(gameName)) {
+    return new Response(`game name may only contain letters and digits!`);
+  }
+
+  const gameStub = env.GAMEINSTANCE.get(env.GAMEINSTANCE.idFromName(gameName));
+  
+  const resp = await gameStub.fetch(req.url);
   return new Response(await resp.text());
 }
